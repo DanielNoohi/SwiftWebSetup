ba07f0c419e5098a7d43f091819f2483ccbfd22b
Mohammad Ali Sadigh Noohi <37836576+DanielNoohi@users.noreply.github.com>
2023-09-21 20:59:58 +0330

    Update web-install.sh

--- web-install.sh
+++ web-install.sh
@@ -153,8 +153,37 @@ fi
 
 sleep 0.5
 
+
+# Check if UFW is installed
+if ! command -v ufw &> /dev/null; then
+    echo "UFW is not installed. Attempting to install..."
+    if [[ $(lsb_release -rs) == "22.04" ]]; then
+        sudo apt update
+        sudo apt install ufw
+    elif [[ $(lsb_release -rs) == "20.04" ]]; then
+        sudo apt update
+        sudo apt install ufw
+    elif [[ $(lsb_release -rs) == "18.04" ]]; then
+        sudo apt update
+        sudo apt install ufw
+    elif [[ -f /etc/redhat-release ]]; then
+        sudo yum install -y ufw
+    else
+        echo "Unsupported Linux distribution. Please install UFW manually."
+        exit 1
+    fi
+
+    # Enable UFW
+    sudo ufw enable
+
+    echo "UFW has been installed and enabled."
+else
+    echo "UFW is already installed."
+fi
+
+
 # Enable the Uncomplicated Firewall (UFW)
-sudo ufw enable
+#sudo ufw enable
 
 # Allow SSH (port 22) for remote access
 sudo ufw allow 22/tcp comment 'Allow SSH Access'
