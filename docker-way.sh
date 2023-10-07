e1884f18625883cdb86c6f8cab3ffdb193eb1e76
Mohammad Ali Sadigh Noohi <37836576+DanielNoohi@users.noreply.github.com>
2023-10-07 14:17:09 +0330

    Update docker-way.sh
    Check if the system needs to reboot

--- docker-way.sh
+++ docker-way.sh
@@ -12,3 +12,29 @@ docker run \
   --publish 80:80 \
   --volume /data:/usr/share/nginx/html \
   nginx
+
+
+# Check if the system needs to reboot
+if [ -f /var/run/reboot-required ]; then
+    echo "The system needs to reboot."
+    
+    # Ask for confirmation to reboot
+    while true; do
+        read -p "Do you want to reboot now? (y/n) " choice
+        case "$choice" in 
+            y|Y ) 
+                echo "Rebooting now..."
+                sudo reboot
+                ;;
+            n|N ) 
+                echo "Reboot cancelled."
+                break
+                ;;
+            * ) 
+                echo "Invalid input. Please enter 'y' or 'n'."
+                ;;
+        esac
+    done
+else
+    echo "The system does not need to reboot."
+fi
